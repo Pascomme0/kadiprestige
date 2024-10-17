881a7cc034369bcfde74b5ad79420d73bbf09805
Paco225 <pacomebrou2003@gmail.com>
2024-10-17 17:13:26 +0000

    heroo

--- app/pages/agencedevoyage/page.jsx
+++ app/pages/agencedevoyage/page.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Banner from '../../components/Banner'
 import FAQSection from '@/app/components/FAQsection'
-import banner from '../../public/banner.jpg'
 import voyage from '../../public/KPvoyage.jpg'
 import KPvoyage from '../../components/KPvoyage'
 
