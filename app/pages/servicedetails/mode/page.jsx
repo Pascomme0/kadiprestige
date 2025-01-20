5eefc3bc38d966f0700b3aba94b8fb655cab3675
Paco225 <pacomebrou2003@gmail.com>
2025-01-20 23:06:30 +0000

    Implement dynamic service details fetching in ServiceDetails page component. Replaced hardcoded service information with API call to retrieve title, description, and image URL, enhancing content management and user experience.

--- app/pages/servicedetails/mode/page.jsx
+++ app/pages/servicedetails/mode/page.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Detailservice from '../../../components/Detailservice'
-import banner from '../../../public/tenue.jpg'
 import Banner from '../../../components/Banner'
 import Pourquoi from '@/app/components/Pourquoi'
 import mode from '../../../public/mode.jpg'
 
 const page = () => {
-  const serviceDetails = {
-    titre: "Création de mode",
-    description: "Chez Kadi Prestige, notre service de création de mode est dédié à l'expression de votre style unique. Notre équipe de designers talentueux travaille en étroite collaboration avec vous pour créer des vêtements sur mesure qui reflètent votre personnalité et répondent à vos besoins spécifiques. Que ce soit pour des tenues de tous les jours, des vêtements de travail élégants ou des créations haute couture pour des occasions spéciales, nous mettons notre expertise à votre service. Nous utilisons des tissus de haute qualité et des techniques de couture avancées pour garantir des finitions impeccables. Notre processus créatif inclut des consultations personnalisées, des essayages minutieux et des ajustements précis pour assurer une coupe parfaite. Avec Kadi Prestige, vivez une expérience de mode unique où votre vision prend vie, créant des pièces intemporelles qui vous feront vous sentir confiant et élégant en toute occasion.",
-    imageUrl: banner,
-    couleurFond: "#ea1d24"
+  const [serviceDetails, setServiceDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchServiceDetails = async () => {
+      try {
+        const response = await fetch('https://admin.kadiprestige.com/api/detail_sections/26');
+        const data = await response.json();
+        setServiceDetails({
+          titre: data.title,
+          description: data.description,
+          imageUrl: `https://admin.kadiprestige.com${data.imagePath}`,
+          couleurFond: "#ea1d24" // Vous pouvez ajuster cette valeur si nécessaire
+        });
+        console.log(`URL de l'image: ${data.imagePath}`); // Vérification de l'URL de l'image
+      } catch (error) {
+        console.error("Erreur lors de la récupération des détails du service:", error);
+      }
+    };
+
+    fetchServiceDetails();
+  }, []);
+
+  if (!serviceDetails) {
+    return <div>Chargement...</div>;
   }
 
   return (
